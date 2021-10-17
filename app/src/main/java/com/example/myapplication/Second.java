b182f59165400787c895c70354beb9e0f5dcdaf7
lun6 <lunlun.jiang@hannto.com>
2021-10-17 16:55:12 +0800

    删除

--- app/src/main/java/com/example/myapplication/Second.java
+++ app/src/main/java/com/example/myapplication/Second.java
@@ -8,6 +8,6 @@ public class Second extends AppCompatActivity {
     private int flag;
     public void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-        setContentView(R.layout.activity_second);
+
     }
 }
