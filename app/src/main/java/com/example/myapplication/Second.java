bc7f11035a4055a2005e61669046b0dc04b96f3d
lun6 <lunlun.jiang@hannto.com>
2021-10-17 16:49:52 +0800

    对second文件做了更改

--- app/src/main/java/com/example/myapplication/Second.java
+++ app/src/main/java/com/example/myapplication/Second.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Second extends AppCompatActivity {
+    private int flag;
     public void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_second);
