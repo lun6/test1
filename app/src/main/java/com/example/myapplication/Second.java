453259e74a732892cd7dc293c20b8feebb7d4383
lun6 <lunlun.jiang@hannto.com>
2021-10-17 16:10:05 +0800

    版本1.1

---
+++ app/src/main/java/com/example/myapplication/Second.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Second extends AppCompatActivity {
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+    }
+}
